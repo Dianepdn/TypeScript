481164c5065d126a4d13c595c1b93f3fd7067bd7
Diane <daipsu@icloud.com>
2018-11-06 19:24:38 -0800

    Add Class inheritance and Method Overriding

--- test2class.js
+++ test2class.js
@@ -30,6 +30,7 @@ console.log("Reading attribute value Engine as : " + obj.engine);
 //access the function
 obj.disp();
 //****/Class Inheritance
+//Single - Every class can at the most extend from one parent class
 var Shape = /** @class */ (function () {
     function Shape(a) {
         this.Area = a;
@@ -48,3 +49,50 @@ var Circle = /** @class */ (function (_super) {
 }(Shape));
 var obj2 = new Circle(200);
 obj2.disp();
+//Multiple - A class can inherit fr multiple classes. TypeScript doesn't support multiple inheritance
+//Multi-level inheritance:
+var Root = /** @class */ (function () {
+    function Root() {
+    }
+    return Root;
+}());
+var Child = /** @class */ (function (_super) {
+    __extends(Child, _super);
+    function Child() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Child;
+}(Root));
+var Leaf = /** @class */ (function (_super) {
+    __extends(Leaf, _super);
+    function Leaf() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Leaf;
+}(Child)); //indirectly inherits fr Root by virtue of inheritance
+var obj3 = new Leaf();
+obj3.str = "hello";
+console.log(obj3.str);
+//Class inheritance and - Method Overriding: is a mechanism by which the child class redefines
+// the superclass's method. Super keyworkd is used to refer to the immediate parent of a class
+var PrinterClass = /** @class */ (function () {
+    function PrinterClass() {
+    }
+    PrinterClass.prototype.doPrint = function () {
+        console.log("doPrint() from Parent called..");
+    };
+    return PrinterClass;
+}());
+var StringPrinter = /** @class */ (function (_super) {
+    __extends(StringPrinter, _super);
+    function StringPrinter() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    StringPrinter.prototype.doWork = function () {
+        _super.prototype.doPrint.call(this); //The super keyword can be used to refer to the super class version of 
+        console.log("doPrint() is printing a string.."); //a variable, property or method
+    };
+    return StringPrinter;
+}(PrinterClass));
+var obj4 = new StringPrinter();
+obj4.doWork();
--- test2class.ts
+++ test2class.ts
@@ -23,6 +23,7 @@ console.log("Reading attribute value Engine as : " + obj.engine)
 obj.disp()
 
 //****/Class Inheritance
+//Single - Every class can at the most extend from one parent class
 class Shape {
     Area:number
     constructor(a:number) {
@@ -36,4 +37,32 @@ class Circle extends Shape {
 }
 var obj2 = new Circle(200)
 obj2.disp()
+
+//Multiple - A class can inherit fr multiple classes. TypeScript doesn't support multiple inheritance
+//Multi-level inheritance:
+class Root {
+    str:string
+}
+class Child extends Root {}
+class Leaf extends Child {} //indirectly inherits fr Root by virtue of inheritance
+var obj3 = new Leaf()
+obj3.str = "hello"
+console.log(obj3.str)
+
+//Class inheritance and - Method Overriding: is a mechanism by which the child class redefines
+// the superclass's method. Super keyworkd is used to refer to the immediate parent of a class
+class PrinterClass {
+    doPrint():void {
+        console.log("doPrint() from Parent called..")
+    }
+}
+class StringPrinter extends PrinterClass {
+    doWork():void {
+        super.doPrint()   //The super keyword can be used to refer to the super class version of 
+        console.log("doPrint() is printing a string..") // ^ a variable, property or method ^
+    }
+}
+var obj4 = new StringPrinter()
+obj4.doWork() // invokes the super class version of the doWork() function
+
 
